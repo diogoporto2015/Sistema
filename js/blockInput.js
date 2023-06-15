739d4e90e57f1da7cb77227695fcd7c9a0353347
DiogoPS <71047847+diogoporto2015@users.noreply.github.com>
2023-06-15 00:43:47 -0300

    at
    TELA DE IMPREÇÃO

---
+++ js/blockInput.js
@@ -0,0 +1,7 @@
+const registro_paciente = document.getElementById('id_paciente');
+const nome = document.getElementById('nome');
+const cpf= document.getElementById('cpf');
+
+registro_paciente.disabled = true
+nome.disabled = true
+cpf.disabled = true
--- js/print.js
+++ js/print.js
@@ -1,13 +1,17 @@
 function imprimirFicha(){
     document.getElementById('dataRecebe').innerHTML = dataAtual();
-    document.getElementById('nomeRecebido').innerHTML =document.getElementById('nome').value;
-    document.getElementById('cpfRecebido').innerHTML =document.getElementById('cpf').value;
-    document.getElementById('nascRecebido').innerHTML =document.getElementById('nasc').value;
-    document.getElementById('endRecebido').innerHTML =document.getElementById('end').value;
-    document.getElementById('telRecebido').innerHTML =document.getElementById('tel').value;
-    document.getElementById('nomeExameRecebido').innerHTML =document.getElementById('nomeExame').value;
-    document.getElementById('tipoExameRecebio').innerHTML =document.getElementById('tipoExame').value;
-    document.getElementById('convRecebido').innerHTML =document.getElementById('conv').value;
+    document.getElementById('nomeRecebido').innerHTML = document.getElementById('nome').value;
+    document.getElementById('cpfRecebido').innerHTML = document.getElementById('cpf').value;
+    document.getElementById('nascRecebido').innerHTML= document.getElementById('data_nascimento').value;
+    document.getElementById('endRecebido').innerHTML = document.getElementById('endereco').value;
+    document.getElementById('numRecebido').innerHTML = document.getElementById('numero').value;
+    document.getElementById('bairroRecebido').innerHTML = document.getElementById('bairro').value;
+    document.getElementById('cidadeRecebido').innerHTML = document.getElementById('cidade').value;
+    document.getElementById('estadoRecebido').innerHTML = document.getElementById('estado').value;
+    document.getElementById('telRecebido').innerHTML = document.getElementById('telefone').value;
+    document.getElementById('nomeExameRecebido').innerHTML = document.getElementById('nome_exame').value;
+    document.getElementById('tipoExameRecebio').innerHTML = document.getElementById('tipo_exame').value;
+    document.getElementById('convRecebido').innerHTML = document.getElementById('convenio').value;
     window.print();
 }
 
